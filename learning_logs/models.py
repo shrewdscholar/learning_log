27e82e16a66f3eb80e807cacb90bbdbe8e4c5c74
lichengjie <3248736823@qq.com>
2026-02-11 14:03:42 +0800

    向管理网站注册了Entry模型

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -14,3 +14,16 @@ class Topic(models.Model):
     def __unicode__(self):
         """返回模型的字符串表示"""
         return self.text
+
+class Entry(models.Model):
+    """学到的有关某个主题的具体知识"""
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entties'
+
+    def __unicode__(self):
+        """返回模型的字符串表示"""
+        return self.text[:50] + "..."
