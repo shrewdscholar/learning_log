7d728167cf283dc04454afe227bd9eacd0bcd616
unknown <lichengjie@360.cn>
2026-02-11 11:01:11 +0800

    添加了创建Entry模型：包含text(Text)、topic(FK)、date_added(auto_now)

---
+++ learning_logs/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+    """用户学习的主题"""
+    text = models.CharField(max_length = 200)
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __unicode__(self):
+        """返回模型的字符串表示"""
+        return self.text
