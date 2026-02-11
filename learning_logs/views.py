d217098824a83662a2906e1012f3059f083c40ff
lichengjie <3248736823@qq.com>
2026-02-11 20:09:51 +0800

    在learning_logs/views.py中添加了对topics的url的设置

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all of the topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
