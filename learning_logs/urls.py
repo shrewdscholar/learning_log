e28998655e211d18b6dffa616ee215446bd46eba
lichengjie <3248736823@qq.com>
2026-02-11 20:13:57 +0800

    在learning_logs/urls.py添加了对topics页面url的设置(上一个其实是对views.py的修改，增加了topics方法，返回topics。html的内容)

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,7 @@ from . import views
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name = 'index'),
+
+    # Show all of the topics
+    url(r'^topics/$', views.topics, name = 'topics'),
 ]
