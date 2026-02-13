7896cfd12dde802c70fb214d996481a8ebda444c
lichengjie <3248736823@qq.com>
2026-02-13 10:55:41 +0800

    创建了一个new_topic方法

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from .models import Topic
+from .forms import TopicForm
 # Create your views here.
 def index(request):
     """学习笔记的主页"""
@@ -19,3 +22,18 @@ def topic(request):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Show a new page to create new topic"""
+    if request.method != 'POST':
+        # Unsubmitted data: create a new form
+        form = TopicForm()
+    else:
+        # POST submitted data, process the data
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResonseRedirect(reverse('learning_logs:topic'))
+    
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
