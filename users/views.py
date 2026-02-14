f061da0f5191bfff571664d118360f53f34b6892
lichengjie <3248736823@qq.com>
2026-02-14 10:43:29 +0800

    在users的views.py中创建了logout_view的方法

--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
 
 # Create your views here.
+def logout_view(request):
+    """Logout user"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
