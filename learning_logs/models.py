5298edb2ffb06f5ecfd91f7ceb769cec92a06e7f
lichengjie <3248736823@qq.com>
2026-02-14 18:10:11 +0800

    在Topic模型中添加了外键owner

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -10,7 +10,7 @@ class Topic(models.Model):
     """用户学习的主题"""
     text = models.CharField(max_length = 200)
     date_added = models.DateTimeField(auto_now_add = True)
-
+    owner = models.ForeignKey(User)
     def __unicode__(self):
         """返回模型的字符串表示"""
         return self.text
