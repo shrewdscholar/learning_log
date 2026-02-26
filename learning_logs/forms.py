aea167ba73bb8b19f2c0db2601a43f8725cff132
lichengjie <3248736823@qq.com>
2026-02-26 19:31:56 +0800

    在新建topic表单中添加public字段

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -7,8 +7,14 @@ from django import forms
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
-        fields = ['text']
-        labels = {'text': ''}
+        fields = ['text', 'public']
+        labels = {
+            'text': '',
+            'public': "Make this topic public?"
+            }
+        widgets = {
+            'public': forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        }
 
 class EntryForm(forms.ModelForm):
     class Meta:
